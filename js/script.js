2c19f4242f5b67b26fc2889c5e193c5cb1533e63
Alex_W <alexcwinter7@gmail.com>
2023-08-23 17:09:36 -0500

    updated button function

--- js/script.js
+++ js/script.js
@@ -4,5 +4,7 @@ var cat = document.querySelector(".cat");
 //console.log(cat);
 
 button.addEventListener("click", function() {
-    cat.classList.add("show")
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show")
+    } else {cat.classList.add("show")}
 });
